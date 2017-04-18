660add8cd8d76503c6acf5e4938366888d4d7a03
JSC54 <jrrscactual@gmail.com>
2017-04-17 19:23:22 -0700

    Test Change
    Change to test that GitHub is functioning

---
+++ PythonGame/PythonGame.py
@@ -0,0 +1,39 @@
+#character creation_start
+
+#stats
+Mean = 0
+Nice = 0
+    #skill points
+Smart = 0
+
+
+
+Player_Name = input('Enter your name: ')
+print('Hello',Player_Name)
+
+#character creation_end
+
+
+#story_start
+print('''
+
+<-----------------Prologue_START----------------->
+
+''')
+print('My name is ' + Player_Name + '.')
+print('''
+      Up until recently, nothing really separated me from everyone else. 
+Although I have fairly good grades, a modest athletic ability, I\'m not really different 
+from anyone else. But remember; I said up until recently. A few days ago, I was watching 
+a TV show called THE FLASH, when I was suddenly struck by a bolt of lightning so strong 
+that it blew a hole right through my roof!. I was on my way to the hospital when one of 
+the first responders tending to me accidentally spilled Gatorade and Powerade and RedBull 
+all at the same time into my IV. I could feel all the energy that was pouring into my body. 
+Long story short, I go to the hospital, they find out I\'m fine, and I go home. Oh, and I 
+have superspeed and stuff.''')
+print('''
+
+<------------------Prologue_END------------------>
+
+''')
+print('test')
